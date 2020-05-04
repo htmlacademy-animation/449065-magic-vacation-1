8dba31540d7803e2632bcaa21d5be070bcec10c3
Yarisson18 <gemini8811@gmail.com>
2020-05-04 22:15:15 +0300

    добавление анимации для шапки, подвала и логотипа

---
+++ source/js/modules/global.js
@@ -0,0 +1,12 @@
+export default () => {
+  document.body.onload = function () {
+    let logo = document.querySelector(`.page-header__logo`);
+    let navigation = document.querySelector(`.page-header__nav`);
+    let footer = document.querySelector(`.screen__footer`);
+
+    logo.classList.add(`page-header__logo--load`);
+    navigation.classList.add(`page-header__nav--load`);
+    footer.classList.add(`screen__footer--load`);
+    // console.log(`страница загружена`);
+  };
+};
